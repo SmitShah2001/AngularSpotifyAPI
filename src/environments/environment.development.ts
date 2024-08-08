08d5b59867458611b7617a46a56fb78d61bacc7b
SmitShah2001 <myhumberlaptop@gmail.com>
2024-08-08 16:17:33 -0400

    minor changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    CLIENTID : "125fd9a425ea4ffd97dd325d37f86624",
+    CLIENTID : "",
 
-    CLIENTSECRET  : "d16528243b1243fcb0caca1fff020290"
+    CLIENTSECRET  : ""
 };
